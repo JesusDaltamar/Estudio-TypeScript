fcc6c08716fc1ab4677ad58d4bc76b768a3b7fce
Jesus Rdoriguez <JesusRodAltamar@gmail.com>
2022-01-28 10:23:26 -0500

    Call back a otra funcion interna

--- function.js
+++ function.js
@@ -6,15 +6,8 @@ const throwError = (message:string) : never=>{
     throw new Error (message);
     return ""; //ESTA FUNCION NUNCA DEVULVE NADA (TIPO NEVER)
 };*/
-var sumar = function (val1, val2) {
-    return val1 + val2;
-};
-var sumar2;
-sumar2 = sumar;
-console.log(sumar2(3, 4));
-var dividir = function (a, b) {
-    return a / b;
-};
-function imprimir() {
-    console.log(sumar(4, 5));
+function imprimir(a, b, mostrar) {
+    var resultado = a + b;
+    mostrar(resultado);
 }
+imprimir(2, 3, function (x) { console.log(x); });
