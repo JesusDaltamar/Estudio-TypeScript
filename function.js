7fb74bc749619c38f510dd3a64c3cfefd3510c22
Jesus Rdoriguez <JesusRodAltamar@gmail.com>
2022-01-28 10:16:25 -0500

    Funcion que referencia a otra funcion

--- function.js
+++ function.js
@@ -1,17 +1,20 @@
 /*function sumar(val1:number ,val2: number) : number {
     return val1 + val2;
-}*/
+}
+
+const throwError = (message:string) : never=>{
+    throw new Error (message);
+    return ""; //ESTA FUNCION NUNCA DEVULVE NADA (TIPO NEVER)
+};*/
 var sumar = function (val1, val2) {
     return val1 + val2;
 };
+var sumar2;
+sumar2 = sumar;
+console.log(sumar2(3, 4));
 var dividir = function (a, b) {
     return a / b;
 };
 function imprimir() {
     console.log(sumar(4, 5));
 }
-imprimir();
-var throwError = function (message) {
-    throw new Error(message);
-    return ""; //nunca se va a retornar nada--tipo "FUNCION TIPO NEVER"
-};
--- function.ts
+++ function.ts
@@ -1,10 +1,19 @@
 /*function sumar(val1:number ,val2: number) : number {
     return val1 + val2;
-}*/
+}
 
+const throwError = (message:string) : never=>{
+    throw new Error (message);
+    return ""; //ESTA FUNCION NUNCA DEVULVE NADA (TIPO NEVER)
+};*/
 const sumar = (val1:number,val2:number):number=>{
     return val1+val2;
-}
+};
+
+let sumar2: (a:number,b:number)=>number;
+sumar2 = sumar;
+
+console.log(sumar2(3,4));
 
 const dividir = function(a:number,b:number):number{
     return a/b;
@@ -14,9 +23,4 @@ function imprimir(): void {
     console.log(sumar(4,5));
 }
 
-imprimir();
 
-const throwError = (message:string) : never=>{
-    throw new Error (message);
-    return ""; //ESTA FUNCION NUNCA DEVULVE NADA (TIPO NEVER)
-};
