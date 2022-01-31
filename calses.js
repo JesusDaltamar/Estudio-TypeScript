3322d33dcc56fa3de831bc950480cc0132236e38
Jesus Rdoriguez <JesusRodAltamar@gmail.com>
2022-01-31 11:28:30 -0500

    Propiedad This

--- calses.js
+++ calses.js
@@ -2,7 +2,16 @@ var Vehicle = /** @class */ (function () {
     function Vehicle(b) {
         this.brandName = b;
     }
+    Vehicle.prototype.drive = function () {
+        console.log("Conduciendo un vehiculo " + this.brandName);
+    };
     return Vehicle;
 }());
 var vehicle = new Vehicle("mazda");
-console.log(vehicle);
+//vehicle.drive();
+//console.log(vehicle);
+var motocicleta = {
+    brandName: "Yamaha",
+    drive: vehicle.drive
+};
+console.log(motocicleta.drive());
--- calses.ts
+++ calses.ts
@@ -3,6 +3,18 @@ class Vehicle{
     constructor(b:string){
         this.brandName = b;
     }
+
+    drive(){
+        console.log("Conduciendo un vehiculo "+ this.brandName)
+    }
 }
+
 const vehicle = new Vehicle("mazda");
-console.log(vehicle);
+//vehicle.drive();
+//console.log(vehicle);
+
+let motocicleta = {
+    brandName:"Yamaha",
+    drive: vehicle.drive
+};
+console.log(motocicleta.drive());
